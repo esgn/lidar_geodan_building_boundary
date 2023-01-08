970d2cd3b382e2c90a6d104110e0051022ac41fb
esgn <5435148+esgn@users.noreply.github.com>
2023-01-08 23:50:49 +0100

    version 2d et 3d du clustering

--- create_footprints_geodan.py
+++ create_footprints_geodan.py
@@ -8,8 +8,16 @@ from shapely.geometry import Polygon
 import matplotlib.pyplot as plt
 import os, shutil
 
+# Input can be cluster taking into account Z or not
+# input_laz = 'cluster3d.laz'
+input_laz = 'cluster2d.laz'
+# output_folder = 'cluster3d_laz_files'
+output_folder = 'cluster2d_laz_files'
+# output_json = 'output3d.geojson'
+output_json = 'output2d.geojson'
+
 # Load cluster.laz and read useful values
-laz = laspy.read('cluster.laz')
+laz = laspy.read(input_laz)
 cluster_min = laz.ClusterID.min()
 cluster_max = laz.ClusterID.max()
 x_scale_factor = laz.header.x_scale
@@ -19,7 +27,6 @@ z_scale_factor = laz.header.z_scale
 polygons = {}
 
 # In order to output cluster data to laz file
-output_folder = "cluster_laz_files"
 if os.path.exists(output_folder):
     shutil.rmtree(output_folder)
     os.mkdir(output_folder)
@@ -30,8 +37,8 @@ else:
 for i in range(cluster_min+1,cluster_max+1):
     
     # To check a specific cluster
-    # if (i!=17):
-    #     continue
+    if (i!=139) and (i!=72):
+        continue
 
     print("dealing with cluster " + str(i))
     cluster = laz.points[laz.ClusterID == i]
@@ -58,10 +65,10 @@ for i in range(cluster_min+1,cluster_max+1):
         try: 
             vertices = building_boundary.trace_boundary(
                 cluster_xy,
-                ransac_threshold=0.5,
-                max_error=0.4,
+                ransac_threshold=0.75,
+                max_error=0.8,
                 alpha=alpha,
-                k=5,
+                k=10,
                 num_points=5,
                 merge_distance=0.6
             )
@@ -75,16 +82,13 @@ for i in range(cluster_min+1,cluster_max+1):
             break
 
     # In order to visualize cluster points and generated footprint
-    # p = Polygon(vertices)
-    # plt.plot(*p.exterior.xy)
-    # plt.scatter(X, Y)
-    # plt.show()
+    p = Polygon(vertices)
+    plt.plot(*p.exterior.xy)
+    plt.scatter(X, Y)
+    plt.show()
 
 print(polygons)
 s = gpd.GeoSeries(polygons.values(),crs="epsg:2154")
-# s.to_file('output.geojson', driver='GeoJSON')
 ids = {'cluster_id':polygons.keys()}
 gdf = gpd.GeoDataFrame(ids, geometry=s, crs="epsg:2154")
-print(gdf)
-# gdf = gpd.GeoDataFrame(polygons.keys(), geometry = polygons.values(), crs="epsg:2154")
-gdf.to_file('output.geojson', driver='GeoJSON')
+gdf.to_file(output_json, driver='GeoJSON')
